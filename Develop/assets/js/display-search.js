2dc698d744a29f30b5e199061aa56b4d8bf45b8f
Joseph Kamil <kamilj@umich.edu>
2024-01-30 23:42:57 -0800

    working api request

--- Develop/assets/js/display-search.js
+++ Develop/assets/js/display-search.js
@@ -1,15 +1,27 @@
 // TODO: Collect a reference to the form with id "search-form"
+var searchFormEl = document.querySelector('#search-form');
 // TODO: Collect a reference to the span with id "result-text"
+var resultTextEl = document.querySelector('#result-text');
 // TODO: Collect a reference to the div with id "result-content"
+var resultContentEl = document.querySelector('#result-content');
+
 
 function getParams() {
 	console.log('document.location:', document.location);
 
 	// TODO: Use the 'search' property of the 'document.location' object to retrieve the search params out of the URL (i.e. `?q=london&format=photo`)
+	var queryString = window.location.search;
+	var searchParams = new URLSearchParams(queryString);
 
 	// TODO: Extract the search term and the format from the string retrieved in the previous step
+	var searchTerm = searchParams.get('q');
+	var format = searchParams.get('format');
+
+	console.log('Search Term:', searchTerm);
+	console.log('Format:', format);
 
 	// TODO: Invoke the searchApi function, passing in the search term and format values as arguments
+	searchApi(searchTerm, format)
 }
 
 getParams();
@@ -17,12 +29,22 @@ getParams();
 function searchApi(searchTerm, format) {
 	console.log('searchTerm:', searchTerm);
 	console.log('format:', format);
-
+  
 	// TODO: Build the request URL
-
+	var apiUrl = "https://www.loc.gov/search/?q=" + searchTerm + "&format=" + format;
+	console.log(apiUrl);
+  
 	// TODO: Make a fetch to the request URL and display the results
+	fetch(apiUrl)
+	  .then(function (response) {
+		console.log(response);
+		if (response.ok) {
+			return response;
+		} else {
+			throw new Error('Error: ' + response.statusText);
+		}
+	  })
 }
-
 function handleNewSearch(event) {
 	event.preventDefault();
 
@@ -34,3 +56,4 @@ function handleNewSearch(event) {
 }
 
 // TODO: Add a 'submit' event listener to the form with id "search-form". A 'submit' event will trigger the 'handleNewSearch' function
+searchFormEl.addEventListener('submit', handleNewSearch);
--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,22 +1,27 @@
 // TODO: Collect a reference to the form with id "search-form"
+var searchFormEl = document.querySelector('#search-form');
 
 function handleSearchFormSubmit(event) {
 	event.preventDefault();
 
 	// TODO: Add a comment to explain the purpose of lines 7 and 8
+	// store the value in the search and format field
 	var searchInputVal = document.querySelector('#search-input').value;
 	var formatInputVal = document.querySelector('#format-input').value;
 
 	// TODO: Add a comment to explain the purpose of lines 11-14
+	// if no value for searchInput say you have an error
 	if (!searchInputVal) {
 		console.error('You need a search input value!');
 		return;
 	}
 
 	// TODO: Add a comment to explain the purpose of lines 17 and 18
+	// create a query in the right format
 	var queryString =
 		'./search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
 	location.assign(queryString);
 }
 
 // TODO: Add a 'submit' event listener to the form with id "search-form". A 'submit' event will trigger the 'handleSearchFormSubmit' function
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
